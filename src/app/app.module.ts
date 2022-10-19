6ffbcca5a3e37e6698fd25b78a08e3b3bb162987
SANDHRAMARIYA <sandhramariya09@gmail.com>
2022-10-19 23:13:16 +0530

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AddlibraryComponent } from './addlibrary/addlibrary.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AddlibraryComponent
   ],
   imports: [
     BrowserModule,
