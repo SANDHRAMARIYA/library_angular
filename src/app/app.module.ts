16107d63c7b83522a27f49f6f40a35cad0abacba
SANDHRAMARIYA <sandhramariya09@gmail.com>
2022-10-19 23:16:52 +0530

    view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AddlibraryComponent } from './addlibrary/addlibrary.component';
+import { ViewlibraryComponent } from './viewlibrary/viewlibrary.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AddlibraryComponent
+    AddlibraryComponent,
+    ViewlibraryComponent
   ],
   imports: [
     BrowserModule,
